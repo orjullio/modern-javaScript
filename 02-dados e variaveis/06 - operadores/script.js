1639bd0a3addc6a931c080130c33ce4b90abf3a0
Rodrigo Júlio Costa  Silva <rjullio@outlook.com>
2025-02-11 20:50:42 -0300

    organização e operadores

---
+++ 02-dados e variaveis/06 - operadores/script.js
@@ -0,0 +1,68 @@
+//operadores aritiméticos
+
+let x;
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;
+
+console.log(x);
+
+//concatenação
+
+x = 'Olá' + ' ' + 'Mundo!!!'; 
+console.log(x);
+
+//expoente
+x = 2 ** 3;
+console.log(x);
+
+//incrementação
+x = 1;
+x = x+1;
+
+console.log(x);
+//
+
+x++;
+console.log(x);
+
+//diminuir
+x = x-1;
+console.log(x);
+
+//operadores
+x = 10;
+x += 5;
+console.log(x);
+x -= 5;
+console.log(x);
+x *= 5;
+console.log(x);
+x /= 5;
+console.log(x);
+x %= 5;
+console.log(x);
+x = 5;
+x **= 5;
+console.log(x);
+
+//operadores de comparação
+x = 2 == 2;
+x = 2 === 2;
+x = 2 === '2'; //false
+x = 2 != 2; // false
+x = 2 !== '2'; // true
+console.log(x);
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+console.log(x);
+
+
+
+
+
