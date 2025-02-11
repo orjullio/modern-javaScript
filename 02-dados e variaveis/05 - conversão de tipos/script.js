e17e3590214bebe6e7754173b4caa946e73667c0
Rodrigo Júlio Costa  Silva <rjullio@outlook.com>
2025-02-11 19:49:09 -0300

    atualização de tipos e conversões.

---
+++ 02-dados e variaveis/05 - conversão de tipos/script.js
@@ -0,0 +1,22 @@
+let amount  =  'olá!';
+
+//convertendo string par number
+amount = parseInt(amount);
+//amount = +amount;
+//amount = Number(amount);
+
+//convertendo numero para string
+//amount = amount.toString();
+//amount = String(amount);
+
+//convertendo string para decimal
+//amount = parseFloat(amount);
+
+//convertendo numero para boolean
+//amount = Boolean(amount);
+console.log(Math.sqrt(-1));  //NaN
+console.log(1 + NaN);
+console.log(undefined + undefined);
+
+console.log(amount, typeof amount);
+
