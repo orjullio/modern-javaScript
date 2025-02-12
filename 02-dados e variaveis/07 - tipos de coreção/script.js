4d2d3691db601a5dae911d6f53bba70eb184ff88
Rodrigo Júlio Costa  Silva <rjullio@outlook.com>
2025-02-12 09:13:41 -0300

    arrumando

---
+++ 02-dados e variaveis/07 - tipos de coreção/script.js
@@ -0,0 +1,16 @@
+//coerção é um tipo de adaptação que o js faz para resolver a solicitação
+
+let x;
+
+x = 5 + '5';
+x = 5 + Number('5');
+x = 5 * '5';
+x = 5 + null;
+x = Number(null);
+x = Number(true);
+x = 5 + true;
+x = 5 * false;
+x = 5 + undefined;
+
+
+console.log(x, typeof x);
