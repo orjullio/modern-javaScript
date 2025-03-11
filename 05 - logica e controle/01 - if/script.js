a8a003526e027a44d5b9d701b17ef9c67397e245
Rodrigo Júlio Costa  Silva <rjullio@outlook.com>
2025-03-11 16:59:13 -0300

    início capítulo 5. lógica e controle.

---
+++ 05 - logica e controle/01 - if/script.js
@@ -0,0 +1,32 @@
+if(true){
+    console.log('isto é verdade')
+}
+
+if(false){
+    console.log('isso é falso');
+}
+
+const x = 10;
+const y = 5;
+
+if(x>y){
+    console.log(`${x} é maior que ${y}`)
+}
+
+if(x===y){
+    console.log(`${x} é igual a ${y}`);
+}else{
+    console.log(`${x} não é igual a ${y}`);
+}
+
+if(x !== y){
+    const z = 20;
+    console.log(`${z} é 20`);
+}
+
+
+//forma abreviada.
+
+if(x >= y) console.log(`${x} é maior ou igual a ${y}`),
+    console.log(`Isso é verdadeiro`);
+    else console.log('Isso é falso');
